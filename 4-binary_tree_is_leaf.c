02c6a877df2de6c9981d58808fce73d85b6b57b1
stevengm45 <stevengm45@gmail.com>
2021-07-21 00:44:59 -0500

    fix, delete return 0

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -13,8 +13,9 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (0);
 	}
 
-	if (node->left == NULL && node->right == NULL && node->parent != NULL)
+	if (node->left == NULL && node->right == NULL)
 	{
 		return (1);
 	}
+	return (0);
 }
