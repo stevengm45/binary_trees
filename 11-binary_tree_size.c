8a438fc605afced46d40e320d0a47d464ba63e1c
ForeroAlexander <foreroalex26@gmail.com>
2021-07-21 00:07:53 -0500

    measures the size of a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,11 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_size - Function that measures the size of a binary tree
+ *
+ * @tree: tree to measure
+ * Return: size of the tree
+ */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
