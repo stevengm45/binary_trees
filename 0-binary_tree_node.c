52e39ec0000640eaa4511d5449d9453b6dc074ec
ForeroAlexander <foreroalex26@gmail.com>
2021-07-21 00:14:08 -0500

    fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,14 +9,20 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	struct binary_tree_s *new;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = new_node->right = NULL;
-	return (new_node);
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+	new->left = NULL;
+
+	return (new);
 }
